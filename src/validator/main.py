1e5a389b576fbee04d120e37a55d4d567c3cacc0
ebienstock <ethan.bienstock@cfpb.gov>
2023-04-27 14:52:02 +0000

    reverted changes to main.py
    The schema still needs to be populated

--- src/validator/main.py
+++ src/validator/main.py
@@ -6,5 +6,59 @@ import pandas as pd
 import pandera as pa
 from schema import sblar_schema
 
+# here is a dataframe containing valid data
+valid_sblar_df = pd.read_excel(
+    "example_sblar.xlsx",
+    engine="openpyxl",
+    sheet_name="valid",
+    dtype=str,
+    na_filter=False,
+)
+
+
+# here is a dataframe containing bad data
+invalid_sblar_df = pd.read_excel(
+    "example_sblar.xlsx",
+    engine="openpyxl",
+    sheet_name="invalid",
+    dtype=str,
+    na_filter=False,
+)
+
+
+def run_validation_on_df(df: pd.DataFrame) -> None:
+    """Run validaition on the supplied dataframe and print a report to
+    the terminal."""
+
+    print("--------------------------------------------------------------------------")
+    print("Performing validation on the following DataFrame.")
+    print("")
+    print(df)
+    print("")
+
+    try:
+        sblar_schema(df, lazy=True)
+    except pa.errors.SchemaErrors as errors:
+        for error in errors.schema_errors:
+            # Name of the column in the dataframe being checked
+            column_name = error["error"].schema.name
+
+            # built in checks such as unique=True are different than custom
+            # checks unfortunately so the name needs to be accessed differently
+            try:
+                check_name = error["error"].check.name
+                # This will either be a boolean series or a single bool
+                check_output = error["error"].check_output
+            except AttributeError:
+                check_name = error["error"].check
+                # this is just a string that we'd need to parse manually
+                check_output = error["error"].args[0]
+
+            print(f"Validation `{check_name}` failed for column `{column_name}`")
+            print(check_output)
+            print("")
+
+
 if __name__ == "__main__":
-    print("I didn't actually do anything but I did run :)")
+    # run_validation_on_df(valid_sblar_df)
+    run_validation_on_df(invalid_sblar_df)
