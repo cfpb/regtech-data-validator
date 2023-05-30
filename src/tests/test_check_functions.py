c30c57b5c1cf0bd591468eb31f6381d9f955ab24
Aldrian Harjati <aldrian.harjati@cfpb.gov>
2023-05-30 16:28:13 -0400

    fixed incorrect example logic

--- src/tests/test_check_functions.py
+++ src/tests/test_check_functions.py
@@ -2,7 +2,7 @@ from validator.check_functions import invalid_date_format
 
 
 class TestInvalidDateFormat:
-    valid_date_format = "202310101"
+    valid_date_format = "20231010"
     
     def test_with_valid_date(self):
-        assert invalid_date_format(self.valid_date_format) == False
+        assert invalid_date_format(self.valid_date_format) == True
