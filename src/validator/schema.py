646091899d67fd1c05d33cd73e0841b8d0f6b5d1
ebienstock <ethan.bienstock@cfpb.gov>
2023-06-16 18:40:08 +0000

    Added valid enum values

--- src/validator/schema.py
+++ src/validator/schema.py
@@ -678,6 +678,16 @@ sblar_schema = DataFrameSchema(
                     accepted_values=[
                         "1",
                         "2",
+                        "3",
+                        "4",
+                        "5",
+                        "6",
+                        "7",
+                        "8",
+                        "9",
+                        "10",
+                        "977",
+                        "999",
                     ],
                 ),
                 SBLCheck(
