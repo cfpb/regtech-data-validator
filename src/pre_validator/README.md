5ed942028ce6b8a74c20aa6431496537b02c1411
ebienstock <ethan.bienstock@cfpb.gov>
2023-06-22 20:56:45 +0000

    WIP

--- src/pre_validator/README.md
+++ src/pre_validator/README.md
@@ -1,5 +1,3 @@
 # Phase 1 Validation
 
-This phase of validation will run before our _validator_ runs.
-
-TODO: THIS NEEDS TO BE UPDATED ONCE THE CODE IS WRITTEN.
+This phase of validation will run before our _validator_ runs. These are probably best thought of as syntactical validations.
---
+++ src/pre_validator/check_functions.py
@@ -0,0 +1,6 @@
+"""Some validation functions to handle phase 1 validation.
+
+Phase 1 validation is the simplest form of validation that verifies the
+supplied data is even close to being correct. For example, numerical 
+fields are actually numbers, enums are numbers and semicolons, etc."""
+
