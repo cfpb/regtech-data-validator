2500f3199e5f42794cc0e449f0d6f14aaccd8494
Nargis Sultani <nargis.sultani@cfpb.gov>
2023-08-03 16:31:37 -0400

    Changed format

--- src/tests/test_check_functions.py
+++ src/tests/test_check_functions.py
@@ -1,22 +1,27 @@
 import pandas as pd
 
 from validator import global_data
-from validator.check_functions import (denial_reasons_conditional_enum_value,
-                                       has_correct_length,
-                                       has_no_conditional_field_conflict,
-                                       has_valid_enum_pair,
-                                       has_valid_fieldset_pair,
-                                       has_valid_format,
-                                       has_valid_multi_field_value_count,
-                                       has_valid_value_count, is_date,
-                                       is_fieldset_equal_to,
-                                       is_fieldset_not_equal_to,
-                                       is_greater_than,
-                                       is_greater_than_or_equal_to,
-                                       is_less_than, is_number,
-                                       is_unique_in_field, is_valid_code,
-                                       is_valid_enum,
-                                       meets_multi_value_field_restriction)
+from validator.check_functions import (
+    denial_reasons_conditional_enum_value,
+    has_correct_length,
+    has_no_conditional_field_conflict,
+    has_valid_enum_pair,
+    has_valid_fieldset_pair,
+    has_valid_format,
+    has_valid_multi_field_value_count,
+    has_valid_value_count,
+    is_date,
+    is_fieldset_equal_to,
+    is_fieldset_not_equal_to,
+    is_greater_than,
+    is_greater_than_or_equal_to,
+    is_less_than,
+    is_number,
+    is_unique_in_field,
+    is_valid_code,
+    is_valid_enum,
+    meets_multi_value_field_restriction,
+)
 
 
 class TestInvalidDateFormat:
