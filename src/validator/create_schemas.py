67a7da986e3a16fed072dc97654bea6d083847fb
Nargis Sultani <nargis.sultani@cfpb.gov>
2023-08-16 10:35:36 -0400

    addressed the comment and modified the phase validation

--- src/validator/create_schemas.py
+++ src/validator/create_schemas.py
@@ -2,16 +2,25 @@
 with validations listed in phase 1 and phase 2."""
 
 from pandera import DataFrameSchema
-from phase_validations import phase_1_and_2_validations
+from phase_validations import get_phase_1_and_2_validations
 from schema_template import get_template
 
 # Get separate schema templates for phase 1 and 2
+
+
 phase_1_template = get_template()
 phase_2_template = get_template()
 
-for column, validations in phase_1_and_2_validations:
-    phase_1_template[column].checks = validations["phase_1"]
-    phase_2_template[column].checks = validations["phase_2"]
 
-phase_1_schema = DataFrameSchema(phase_1_template)
-phase_2_schema = DataFrameSchema(phase_2_template)
+def get_template_by_phase(template, validation, lei: str):
+    for column, validations in get_phase_1_and_2_validations(lei):
+        template[column].checks = validations[validation]
+    return DataFrameSchema(template)
+
+
+def get_phase_1_schema(lei: str):
+    return get_template_by_phase(phase_1_template, "phase_1", lei)
+
+
+def get_phase_2_schema(lei: str):
+    return get_template_by_phase(phase_2_template, "phase_2", lei)
