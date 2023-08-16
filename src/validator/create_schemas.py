031b4c9cd22dc4fb69a00213d0f3900cb534f4c8
Nargis Sultani <nargis.sultani@cfpb.gov>
2023-08-16 10:39:47 -0400

    modified function name

--- src/validator/create_schemas.py
+++ src/validator/create_schemas.py
@@ -12,15 +12,15 @@ phase_1_template = get_template()
 phase_2_template = get_template()
 
 
-def get_template_by_phase(template, validation, lei: str):
+def get_schema_by_phase(template, validation, lei: str):
     for column, validations in get_phase_1_and_2_validations(lei):
         template[column].checks = validations[validation]
     return DataFrameSchema(template)
 
 
 def get_phase_1_schema(lei: str):
-    return get_template_by_phase(phase_1_template, "phase_1", lei)
+    return get_schema_by_phase(phase_1_template, "phase_1", lei)
 
 
 def get_phase_2_schema(lei: str):
-    return get_template_by_phase(phase_2_template, "phase_2", lei)
+    return get_schema_by_phase(phase_2_template, "phase_2", lei)
