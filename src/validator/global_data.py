138fc440c90f30a1095012013280a0ff524617a4
Aldrian Harjati <aldrian.harjati@cfpb.gov>
2023-09-15 09:10:00 -0400

    update deprecated pandas function

--- src/validator/global_data.py
+++ src/validator/global_data.py
@@ -14,23 +14,23 @@ naics_codes = {}
 census_geoids = {}
 
 
-def read_naics_codes():
+def read_naics_codes(csv_path: str = NAICS_CSV_PATH):
     """
     read NAICS CSV file with this format: (code, description)
     and populate global value: naics_codes
     """
     naics_codes.clear()
-    df = pd.read_csv(NAICS_CSV_PATH, dtype=str, na_filter=False)
+    df = pd.read_csv(csv_path, dtype=str, na_filter=False)
     for _, row in df.iterrows():
-        naics_codes.update({row[0]: row[1]})
+        naics_codes.update({row.iloc[0]: row.iloc[1]})
 
 
-def read_geoids():
+def read_geoids(csv_path: str = CENSUS_PROCESSED_CSV_PATH):
     """
     read geoids CSV file with this format: (code)
     and populate global value: census_geoids
     """
     census_geoids.clear()
-    df = pd.read_csv(CENSUS_PROCESSED_CSV_PATH, dtype=str, na_filter=False)
+    df = pd.read_csv(csv_path, dtype=str, na_filter=False)
     for _, row in df.iterrows():
-        census_geoids.update({row[0]: None})
+        census_geoids.update({row.iloc[0]: None})
