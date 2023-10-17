b732419bf0d077a49685539f247747ac8bc088aa
Hans Keeler <hans.keeler@cfpb.gov>
2023-10-17 03:21:12 -0400

    Print CLI output as JSON instead of Python dict.

--- regtech_data_validator/main.py
+++ regtech_data_validator/main.py
@@ -5,7 +5,7 @@ the SBL Pandera schema.
 Run from the terminal to see the generated output.
 """
 
-import pprint
+import json
 import sys
 
 import pandas as pd
@@ -17,18 +17,21 @@ def csv_to_df(path: str) -> pd.DataFrame:
     return pd.read_csv(path, dtype=str, na_filter=False)
 
 
-def run_validation_on_df(df: pd.DataFrame, lei: str) -> None:
+def run_validation_on_df(df: pd.DataFrame, lei: str|None) -> None:
     """
     Run validation on the supplied dataframe and print a report to
     the terminal.
     """
 
-    pprint.pprint(validate_phases(df, lei))
+    validation_dict = validate_phases(df, lei)
+    validation_json = json.dumps(validation_dict, indent=4)
+
+    print(validation_json)
 
 
-if __name__ == "__main__":
+def main():
     csv_path = None
-    lei: str = None
+    lei: str|None = None
     if len(sys.argv) == 1:
         raise ValueError("csv_path arg not provided")
     elif len(sys.argv) == 2:
@@ -41,3 +44,6 @@ if __name__ == "__main__":
 
     df = csv_to_df(csv_path)
     run_validation_on_df(df, lei)
+
+if __name__ == "__main__":
+    main()
